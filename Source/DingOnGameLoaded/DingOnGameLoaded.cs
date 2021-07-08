9727e43d26826d49c415ac0cefc53def69b4596b
emipa606 <emil.offermo@litet.net>
2021-07-08 22:28:20 +0200

    Mod updated for 1.3 and passed autotests

--- Source/DingOnGameLoaded/DingOnGameLoaded.cs
+++ Source/DingOnGameLoaded/DingOnGameLoaded.cs
@@ -1,15 +1,37 @@
 ﻿using System.Reflection;
 using HarmonyLib;
 using Verse;
+using Verse.Sound;
 
 namespace DingOnGameLoaded
 {
-    internal class DingOnGameLoaded : Mod
+    public class DingOnGameLoaded : Mod
     {
+        private static bool HavePlayedDing;
+
         public DingOnGameLoaded(ModContentPack content) : base(content)
         {
             var harmony = new Harmony("Mlie.DingOnGameLoaded");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void PlayDing()
+        {
+            if (HavePlayedDing)
+            {
+                return;
+            }
+
+            var soundDef = DefDatabase<SoundDef>.GetNamedSilentFail("DingOnGameLoaded");
+            if (soundDef == null)
+            {
+                Log.Message("[DingOnGameLoaded]: No sound-file found");
+                return;
+            }
+
+            //Log.Message("[DingOnGameLoaded]: Playing sound");
+            soundDef.PlayOneShot(SoundInfo.OnCamera());
+            HavePlayedDing = true;
         }
     }
 }
---
+++ Source/DingOnGameLoaded/RimWorld_MainMenuDrawer_MainMenuOnGUI.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace DingOnGameLoaded
+{
+    [HarmonyPatch(typeof(MainMenuDrawer), "MainMenuOnGUI")]
+    public static class RimWorld_MainMenuDrawer_MainMenuOnGUI
+    {
+        /// <summary>
+        ///     This is the best place for games with the run in background is turned on.
+        /// </summary>
+        [HarmonyPostfix]
+        public static void MainMenuOnGUI()
+        {
+            //Log.Message("[DingOnGameLoaded]: Last try to play sound");
+            DingOnGameLoaded.PlayDing();
+        }
+    }
+}
